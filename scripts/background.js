6085aa810137f3ddc19c40a78915ccd645120cce
hrmtsh2 <144373394+hrmtsh2@users.noreply.github.com>
2024-03-02 22:47:34 +0530

    Update background.js with activeTabUrl printing

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,10 @@
+console.log("Inside background script");
 
 let activeTabUrl = "";
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.activeTabUrl){
         activeTabUrl = message.activeTabUrl;
+        console.log(activeTabUrl);
     }  
 }
