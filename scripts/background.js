ec2a196488f0371535216573437d4f0224970141
hrmtsh2 <144373394+hrmtsh2@users.noreply.github.com>
2024-03-03 06:54:35 +0530

    Update background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,13 +1,21 @@
 console.log("Inside background script");
 
 let activeTabUrl = "";
-let clipboardData = '';
+let allTabsLinks = [];
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.activeTabUrl) {
-        activeTabUrl = message.activeTabUrl;
-        console.log(activeTabUrl);
-    }
+	if(message.activeTabUrl){
+		activeTabUrl = message.activeTabUrl;
+		console.log(activeTabUrl);
+	}
+	if(message.allTabsLinks){
+		allTabsLinks = message.allTabsLinks;
+	}
+	if(message.currentTabButtonClicked){
+		console.log("Sending this to indexScript.js - " + activeTabUrl);
+		sendResponse({activeTabLink: activeTabUrl});
+	}
+	if(message.allTabsButtonClicked){
+		console.log("Sending this to indexScript.js - " + activeTab);
+	}
 })
-
-
